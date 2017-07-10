e4d7b93fa54e52ba1a3b756f85dbc5cbf73d31b2
ICNCAST <ICNCAST@DESKTOP-5S6JQQE>
2017-07-10 11:40:34 +0900

    코드 재구축 완료

--- src/Explorer.java
+++ src/Explorer.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /*   탐색기 클래스   */
@@ -34,41 +36,52 @@ public class Explorer {
 	
 	/*   복합요소 객체, 소켓스레드 객체를 사용하는 파일 목록 생성 메소드, 동작, 숫자포맷, IOException   */
 	public static void makeFileList(Component component, SocketThread socketThread) throws UnsupportedOperationException, NumberFormatException, IOException {
-		// 소켓스레드를 이용, 현재 디렉토리 안내 메세지 전송
-		// 콘솔에서 입력하는 메뉴 선택 번호 저장
-		// 루트 복합요소 객체의 태그를 이용한 파일 객체 생성
-		// 파일리스트에 루트 복합요소 객체의 목록 저장
-		// 파일리스트의 크기 저장
-		// 파일리스트의 크기만큼 반복
-		// 현재 접근중인 루트 복합요소의 자식 요소 객체 저장
-		// 자식요소의 태그가 "F: "와 같을 때
-		// 소켓스레드를 이용, 번호와 태그, 값을 전송
-		// 아니라면 (디렉토리라면)
-		// 현재 접근중인 자식 요소의 태그 저장
-		// 태그에서 경로기호 "\\"의 인덱스 확인
-		// 소켓스레드를 이용, 번호, 디렉토리 태그 직접 입력, 경로기호 이후의 값만 잘라내서 값을 전송
+		socketThread.sendMessage("현재 디렉토리 : " + component.getTag());// 소켓스레드를 이용, 현재 디렉토리 안내 메세지 전송
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 콘솔에서 입력하는 메뉴 선택 번호 저장
+		File file = new File(component.getTag());// 루트 복합요소 객체의 태그를 이용한 파일 객체 생성
+		File[] fileList = file.listFiles();// 파일리스트에 루트 복합요소 객체의 목록 저장
+		int size = fileList.length;// 파일리스트의 크기 저장
+		for(int i = 0 ; i < size ; i++) {// 파일리스트의 크기만큼 반복
+			Component temp = component.getChild(i);// 현재 접근중인 루트 복합요소의 자식 요소 객체 저장
+			if(temp.getTag().equals("F: ")) {// 자식요소의 태그가 "F: "와 같을 때
+				socketThread.sendMessage(i + 1 +". " + temp.getTag() + temp.getValue());// 소켓스레드를 이용, 번호와 태그, 값을 전송
+			}
+			else {// 아니라면 (디렉토리라면)
+				String tempRoute = temp.getTag();// 현재 접근중인 자식 요소의 태그 저장
+				int criteria = tempRoute.lastIndexOf("\\");// 태그에서 경로기호 "\\"의 인덱스 확인
+				socketThread.sendMessage(i + 1 + ". D:" + tempRoute.substring(criteria));// 소켓스레드를 이용, 번호, 디렉토리 태그 직접 입력, 경로기호 이후의 값만 잘라내서 값을 전송
+			}
+		}
 	}
 	
 
 	/*   번호, 복합요소 객체, 소켓스레드를 이용한 메뉴 선택 메소드, 동작, IOException   */
-	// 해당 번호의 복합요소 객체 자식 요소 객체를 저장
-	// 해당 번호에서 1 감소한 자식 요소 객체를 저장
-	// 해당 자식요소의 태그가 "F: "와 같다면(파일이라면)
-	// 루트 복합요소객체의 태그(경로)와 자식요소의 값을 더해서 경로 생성
-	// 새로운 경로를 이용하여 파일 객체 생성
-	// 현재 선택된 파일이 읽을 수 있는 파일인지 확인
-	// 소켓스레드를 이용, 선택한 번호와 경로 안내 메세지 전송
-	// 루트 복합요소 객체와 소켓스레드를 이용하여 파일목록 생성
-	// 딜레이
-	// 읽기 불가능한 파일이라면 
-	// 소켓스레드를 이용, 읽기 불가 안내 메세지 전송
-	// 루트 복합요소 객체와 소켓스레드를 이용하여 파일목록 생성 
-	// 딜레이
-	// 해당 자식요소가 디렉토리라면
-	// 소켓스레드를 사용하여 해당 디렉토리 선택 안내메세지 전송 
-	// 선택 디렉토리, 소켓스레드를 이용하여 파일목록 생성
-	// 딜레이
+	public void selectMenu(int num, Component root, SocketThread socketThread) throws UnsupportedOperationException, IOException {
+		Component temp1 = root.getChild(num);// 해당 번호의 복합요소 객체 자식 요소 객체를 저장
+		Component temp2 = root.getChild(num - 1);// 해당 번호에서 1 감소한 자식 요소 객체를 저장
+		if(temp2.getTag().equals("F: ")) {// 해당 자식요소의 태그가 "F: "와 같다면(파일이라면)
+			String route = root.getTag() + "/" + temp2.getValue();// 루트 복합요소객체의 태그(경로)와 자식요소의 값을 더해서 경로 생성
+			File file = new File(route);// 새로운 경로를 이용하여 파일 객체 생성
+			if(file.canRead()) {// 현재 선택된 파일이 읽을 수 있는 파일인지 확인
+				socketThread.sendMessage("선택한 항목은 " + num + 1 + ". " + route);// 소켓스레드를 이용, 선택한 번호와 경로 안내 메세지 전송
+				makeFileList(root, socketThread);// 루트 복합요소 객체와 소켓스레드를 이용하여 파일목록 생성
+				delay();// 딜레이
+			}
+			else {// 읽기 불가능한 파일이라면
+				socketThread.sendMessage("읽기 불가능한 파일입니다");// 소켓스레드를 이용, 읽기 불가 안내 메세지 전송
+				makeFileList(root, socketThread);// 루트 복합요소 객체와 소켓스레드를 이용하여 파일목록 생성 
+				delay();// 딜레이 
+			}
+		}
+		else {// 해당 자식요소가 디렉토리라면
+			socketThread.sendMessage(num + 1 + "번을 선택하셨습니다. 해당 디렉토리로 이동합니다");// 소켓스레드를 사용하여 해당 디렉토리 선택 안내메세지 전송 
+			makeFileList(temp2, socketThread);// 선택 디렉토리, 소켓스레드를 이용하여 파일목록 생성
+			delay();// 딜레이
+		}
+	}
 
 	/*   강제 딜레이 메소드   */
-	// 900000000번 for문 반복으로 강제 딜레이 생성
+	public void delay() {
+		for(int i = 0 ; i < 900000000 ; i++) {}// 900000000번 for문 반복으로 강제 딜레이 생성
+	}
 }
--- src/SocketThread.java
+++ src/SocketThread.java
@@ -1,6 +1,9 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +35,7 @@ public class SocketThread extends Thread {
 			explorer.makeFileList(root, this);// 복합요소 root 객체와 소켓을 사용하여 하위 요소 목록 생성
 			while(true) {// 계속 반복
 				try {// 내각 try
-					// 메뉴선택 메소드 호출(선택 메뉴, root 객체, 소켓)
+					explorer.selectMenu(menuSelect, root, this);// 메뉴선택 메소드 호출(선택 메뉴, root 객체, 소켓)
 				} catch(IOException e) {// 내각 catch IOException
 					socket.close();// 소켓 닫기
 					threadList.remove(socket);// 스레드에서 해당 소켓 제거
@@ -46,38 +49,44 @@ public class SocketThread extends Thread {
 
 	/*   선택한 메뉴값, 복합요소, 소켓스레드를 이용한 메뉴 선택 메소드, 동작, IO Exception 발생  */
 	public void selectMenu(int menuSelect, Component componet) throws UnsupportedOperationException, IOException {
-		InputStream in;// 인풋스트림 선언
+		InputStream in = null;// 인풋스트림 선언
 		// 소켓스레드로 디렉토리, 파일 선택 안내 메세지 출력
 		// 소켓으로 전달받은 인풋스트림 저장
 		String message;// 메세지 저장 변수 선언
 		int size;// 크기 선언
-		Byte[] byteArray = new Byte[10240];// 전송 위한 크기가 10240인 바이트배열 선언
-		// 인풋스트림으로 받은 내용의 크기를 바이트배열을 이용하여 저장
-		// 내용 크기가 0이하일 때 IO exception 발생
-		// String 객체를 이용하여 메세지 저장(매개변수 4개짜리 String 확인 요)
-		//메세지 전후 공백 제거
-		// 메세지를 int형으로 변환하여 메뉴 선택 변수에 저장
+		byte[] byteArray = new byte[10240];// 전송 위한 크기가 10240인 바이트배열 선언
+		size = in.read(byteArray);// 인풋스트림으로 받은 내용의 크기를 바이트배열을 이용하여 저장
+		if(size <= 0) {// 내용 크기가 0이하일 때 IO exception 발생
+			throw new IOException();
+		}
+		message = new String(byteArray, 0, size, "UTF-8");// String 객체를 이용하여 메세지 저장(매개변수 4개짜리 String 확인 요)
+		message = message.trim();// 메세지 전후 공백 제거
+		menuSelect = Integer.parseInt(message);// 메세지를 int형으로 변환하여 메뉴 선택 변수에 저장
 		// 탐색기의 메뉴 선택 메소드 호출
 	}
 	
 	/*   복합요소 객체, 소켓스레드를 받는 하위 요소 목록 생성 메소드, 숫자포맷, 동작, IO Exception 발생   */
-	// 탐색기 객테의 하위 요소 목록 생성 메소드 호출
+	// 탐색기 객체의 하위 요소 목록 생성 메소드 호출
 
 	/*   살아있는 전체 소켓 메세지 전달 메소드   */
-	public void sendMessage() {
+	public void sendMessageAll(String message) {
 		SocketThread socketThread = this;// 소켓스레드 선언
 		for(int i = 0 ; i < this.threadList.size() ; i++) {// 스레드의 크기만큼 반복
 			socketThread = this.threadList.get(i);// 현재 접근중인 소켓스레드의 값 저장
 			if(socketThread.isAlive()) {// 소켓이 살아있는지 확인
-				// 메세지 전송
+				sendMessage(message);// 메세지 전송
 			}
 		}
 	}
 
 	/*   특정 소켓에 메세지 전달 메소드   */
-	// try
-	// 소켓의 OutputStream을 이용하여 PrintWriter 객체 선언
-	// 메세지 출력
-	// 객체 초기화
-	// catch IOException
+	public void sendMessage(String message) {
+		try {// try
+			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);// 소켓의 OutputStream을 이용하여 PrintWriter 객체 선언
+			out.println(message);// 메세지 출력
+			out.flush();// 객체 초기화
+
+		} catch(IOException e) {// catch IOException
+		}
+	}
 }
